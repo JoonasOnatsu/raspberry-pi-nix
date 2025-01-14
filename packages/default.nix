3ae5d3dbd73123c97ee17308fd81591f1d91b0b8
Joonas Onatsu <joonas.onatsu@unikie.com>
2025-01-14 23:28:34 +0200

    fix wireless firmware package

--- packages/default.nix
+++ packages/default.nix
@@ -5,5 +5,5 @@
   libcamera = pkgs.callPackage ./libcamera {inherit libpisp;};
   #dtmerger = pkgs.callPackage ./dtmerger.nix {};
   rpicam-apps = pkgs.callPackage ./rpicam-apps.nix {inherit libpisp libcamera;};
-  #rpi-wireless-firmware = pkgs.callPackage ./rpi-wireless-firmware.nix {};
+  raspberrypi-wireless-firmware = pkgs.callPackage ./raspberrypi-wireless-firmware.nix {};
 }
