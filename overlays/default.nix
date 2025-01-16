fa4bb900c6fc4969528246b69bad5febd22c8837
Joonas Onatsu <joonas.onatsu@unikie.com>
2025-01-16 16:08:05 +0200

    override formatting, fix pixman build

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,8 +12,26 @@
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
     unbound = prev.unbound.overrideAttrs (
-      oldAttrs: {
-        nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [final.buildPackages.bison];
+      finalAttrs: previousAttrs: {
+        nativeBuildInputs = (previousAttrs.nativeBuildInputs or []) ++ [final.buildPackages.bison];
+      }
+    );
+
+    # Pixman from stable 24.11 (v0.43.4) won't build
+    # on armv6/armv7, but the version (v0.44.2) from
+    # unstable builds, so make an override to change
+    # the derivation to same as in unstable.
+    pixman = prev.pixman.overrideAttrs (
+      finalAttrs: previousAttrs: {
+        version = "0.44.2";
+        src = final.fetchurl {
+          urls = with finalAttrs; [
+            "mirror://xorg/individual/lib/${pname}-${version}.tar.gz"
+            "https://cairographics.org/releases/${pname}-${version}.tar.gz"
+          ];
+          hash = "sha256-Y0kGHOGjOKtpUrkhlNGwN3RyJEII1H/yW++G/HGXNGY=";
+        };
+        mesonFlags = [];
       }
     );
   };
